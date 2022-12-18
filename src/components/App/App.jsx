3e0c0c3e3ebfb657fb288c40a3a7a69dc71ae082
GannaKov <zlatta2000@gmail.com>
2022-12-18 22:25:13 +0200

    App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 import { Toaster } from 'react-hot-toast'
 import { GlobalStyle } from 'CreateGlobalStyle';
 import { Searchbar } from 'components/Searchbar/Searchbar';
@@ -8,9 +8,51 @@ import { AppDiv } from './App.styled';
 
 // import { fetchPhotos } from 'components/FetchApi';
 
+export const App =()=>{
+  const [page, setPage] = useState(1);
+  const [searchWord, setSearchWord] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [largeImgData, setLargeImgData] = useState({});//!!!!largeImgData: { src: '', alt: '' },
+ // const [status, setStatus] = useState("");
 
+const  toggleModal=()=>{setShowModal(showModal => !showModal)}
+ 
+ const handleFormSubmit = searchWord => {
+    setSearchWord( searchWord)
+    setPage(1) };
 
-export class App extends Component {
+   const shereSrcForModal=(srcLarge,altLarge)=>{ 
+    const newLargeImgData ={src:srcLarge, alt:altLarge}
+      setLargeImgData(newLargeImgData)
+    }
+    const loadMore = () => {
+      setPage(state => state + 1);
+    };
+  return (
+    <AppDiv><GlobalStyle />
+    <Toaster toastOptions={{
+      
+      error: {duration: 3000,
+        style: { border: '1px solid red',
+        padding: '16px',
+        
+          minWidth: '450px',
+         
+        },
+      },
+    }}/>
+    <Searchbar onSubm={handleFormSubmit}/>
+  <ImgGallery searchWord={searchWord} loadMore={loadMore} page={page}
+  onImgClick={toggleModal} shereSrcForModal={shereSrcForModal} 
+ />
+ 
+  
+  {showModal &&(<Modal src={largeImgData.src} alt={largeImgData.alt} 
+  onClose={toggleModal}/>)}
+  </AppDiv>)
+}
+//--------
+export class oldApp extends Component {
   state = { page: 1,
     searchWord:"",
   showModal:false,
